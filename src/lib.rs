8e665ea25e3bfa933c1d570f9cfc1bcbedde6ea2
Neil Deo <neildeo42@gmail.com>
2026-04-30 07:49:03 +0100

    Add metadata smoke test in place of default test

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn crate_exposes_package_metadata() {
+        let name = env!("CARGO_PKG_NAME");
+        let version = env!("CARGO_PKG_VERSION");
+
+        assert_eq!(name, "minidf");
+        assert!(!version.is_empty());
     }
 }
